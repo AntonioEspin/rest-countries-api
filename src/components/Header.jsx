2e2ee17d8756456c2b989476af58a04db10701bb
AntonioEspin <antoesdev91@gmail.com>
2024-02-26 18:32:55 -0500

    Add data.json and component Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/Header.css'
+
+export function Header () {
+  return (
+    <header className="header">
+      <h1>Where in the world?</h1>
+      <div className="header-darkMode">
+        <img src="" alt="icon-moon" />
+        <span>Dark Mode</span>
+      </div>
+    </header>
+  )
+}
